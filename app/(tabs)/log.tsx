af55a8d5ac33c10db36a4516db0282f0322366f1
Igor Roberto Ribeiro - FATEC - Cuiaba/MT <00602779@alunosenai.mt>
2026-02-24 21:34:05 -0400

    first commit

---
+++ app/(tabs)/log.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+export default function Login(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Login</Text>
+            <TextInput style={styles.input}/>
+            <TextInput style={styles.input} secureTextEntry={true}/>
+            <TouchableOpacity style={styles.botao}>
+                <Text style={styles.titulo}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:'#ADD8E6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    titulo:{
+        fontFamily: 'arial',
+        marginBottom: 20,
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    input:{
+        width: '100%',
+        height: 45,
+        backgroundColor: '#ffffff',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+    botao:{
+        width: '70%',
+        height: 40,
+        backgroundColor: '#000080',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+    }
+})
