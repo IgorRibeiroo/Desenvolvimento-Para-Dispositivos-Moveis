87b38d8f1cc89b16a7e7e66002f9f39778747ca1
Igor Roberto Ribeiro - FATEC - Cuiaba/MT <00602779@alunosenai.mt>
2026-03-03 20:58:51 -0400

    Atualização no botão do login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+export default function Login(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulologin}>Login</Text>
+            <TextInput style={styles.input}/>
+            <TextInput style={styles.input} secureTextEntry={true}/>
+            <TouchableOpacity style={styles.botao}>
+                <Text style={styles.titulosubmit}>Entrar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:'#ADD8E6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    titulologin:{
+        fontFamily: 'arial',
+        marginBottom: 20,
+        fontSize: 20,
+        color: 'black',
+        fontWeight: 'bold'
+    },
+    titulosubmit:{
+        fontFamily: 'arial',
+        marginBottom: 20,
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    input:{
+        width: '100%',
+        height: 45,
+        backgroundColor: '#ffffff',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+    botao:{
+        width: '70%',
+        height: 40,
+        backgroundColor: '#000080',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+    }
+})
